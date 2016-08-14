2e08c67937f1b24880a7b67748f9d24f366e3385
luxiaofeng <442171865@qq.com>
2016-08-14 14:17:12 +0800

    修改了demo3按钮倒计时的内存溢出问题

--- BABaseProject/BAMainCode/BACode/Home(首页)/DemoVC/DemoVC3/DemoVC3.m
+++ BABaseProject/BAMainCode/BACode/Home(首页)/DemoVC/DemoVC3/DemoVC3.m
@@ -23,6 +23,8 @@
 @property (nonatomic, strong) SXHeadLine  *headerLine;
 @property (nonatomic, strong) SXMarquee   *headerLine2;
 
+@property (strong, nonatomic) dispatch_source_t timer2;
+
 @end
 
 @implementation DemoVC3
@@ -37,6 +39,8 @@
     [super viewWillDisappear:animated];
     
     [_headerLine stop];    
+    
+    dispatch_source_cancel(self.timer2);
     
 //    _timeButton2 = nil;
 //    _timeButton3 = nil;
@@ -126,16 +130,16 @@
     
     /*! 倒计时时间 */
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-    dispatch_source_t _timer2 = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0,queue);
-    dispatch_source_set_timer(_timer2,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0);
+    self.timer2 = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0,queue);
+    dispatch_source_set_timer(self.timer2,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0);
     
     /*! 每秒执行 */
-    dispatch_source_set_event_handler(_timer2, ^{
+    dispatch_source_set_event_handler(self.timer2, ^{
         
         if(timeout <= 0)
         {
             /*! 倒计时结束，关闭 */
-            dispatch_source_cancel(_timer2); dispatch_async(dispatch_get_main_queue(), ^{
+            dispatch_source_cancel(self.timer2); dispatch_async(dispatch_get_main_queue(), ^{
                 
                 /*! 设置界面的按钮显示 根据自己需求设置 */
                 [btn setTitle:@"发送验证码" forState:UIControlStateNormal];
@@ -165,7 +169,7 @@
             timeout--;
         }
     });
-    dispatch_resume(_timer2);
+    dispatch_resume(self.timer2);
 }
 
 #pragma mark - ***** 第3种button
@@ -184,11 +188,6 @@
         [self.view addSubview:_timeButton3];
     }
     return _timeButton3;
-}
-
-- (BOOL)willDealloc
-{
-    return NO;
 }
 
 - (IBAction)btnAction3:(UIButton *)sender
