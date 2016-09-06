43af14d1e90b2734c4c4376af5c51c1dc2605ec4
luxiaofeng <442171865@qq.com>
2016-09-06 09:22:22 +0800

    增加了请求封装的请求数据格式设置,和返回数据格式设置

--- BABaseProject/BAMainCode/BATools(工具类)/BANetManager(网络类)/BANetManager.m
+++ BABaseProject/BAMainCode/BATools(工具类)/BANetManager(网络类)/BANetManager.m
@@ -115,10 +115,15 @@ static NSMutableArray *tasks;
         manager.requestSerializer.cachePolicy = NSURLRequestReloadIgnoringLocalCacheData;
         
         /*! 设置返回数据为json, 分别设置请求以及相应的序列化器 */
-        manager.responseSerializer = [AFJSONResponseSerializer serializer];
-        
+        //根据服务器的设定不同还可以设置 [AFHTTPResponseSerializer serializer](常用)
         AFJSONResponseSerializer * response = [AFJSONResponseSerializer serializer];
         response.removesKeysWithNullValues = YES;
+        manager.responseSerializer = response;
+        
+        /* 设置请求服务器数据格式为json */
+        //根据服务器的设定不同还可以设置 [AFHTTPRequestSerializer serializer](常用)
+        AFJSONRequestSerializer * request = [AFJSONRequestSerializer serializer];
+        manager.requestSerializer = request;
         
         /*! 设置apikey ------类似于自己应用中的tokken---此处仅仅作为测试使用*/
         //        [manager.requestSerializer setValue:apikey forHTTPHeaderField:@"apikey"];
